7701ec7f3db3f443ecea3770069d4a110d95c415
gntodtndls156 <gntodtndls156@gmail.com>
2024-04-03 11:26:29 +0900

    feat: add save file system

---
+++ bookFile.py
@@ -0,0 +1,27 @@
+# type
+from bookRecommender import RecommendBook
+
+def saveFile(filename, book: RecommendBook):
+    title = book.get_name()
+    writer = book.get_writer()
+    body = book.get_bInfo()
+    
+    with open(filename + '.txt', 'w', encoding='UTF-8') as f:
+        f.writelines(f"""제목: 책 추천
+안녕하세요!
+
+제가 추천하는 책 정보입니다.
+
+===================================
+책 제목: {title}
+저자: {writer}
+
+책 소개:
+{body}
+
+책 링크: [책 링크]
+===================================
+
+이 책을 읽어보시면 좋을 것 같습니다. 궁금한 점이 있다면 언제든 연락주세요.
+
+감사합니다.""")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bookRecommender import getBook
 from bookEmail import sendEmail
+from bookFile import saveFile
 
 # init variables
 enumGenre = {'소설': '01', '시/에세이': '03', '인문':'05','가정/육아':'07','요리':'08','건강':'09','취미/실용/스포츠':'11', '경제/경영':'13', '자기계발':'15', '정치/사회':'17', '역사/문화':'23', '종교':'21','예술/대중문화':'23','중/고등참고서':'25', '기술/공학': '26', '외국어':'27','과학':'29', '취업/수험서':'31', '여행':'32', '컴퓨터/IT':'33', '잡지':'35', '청소년':'38', '초등참고서':'39', '유아(0~7세)': '41', '어린이(초등)': '42', '만화': '47', '대학교재': '50', '한국소개도서': '53', '교보오리지널':'59'}
@@ -25,14 +26,24 @@ book = getBook(genre)
 print('책 선택 완료!')
 
 # input
-way = input('오늘의 책을 어떻게 받으시겠어요? 메일 / 파일 : ')
+try :
+    way = input('오늘의 책을 어떻게 받으시겠어요? 메일 / 파일 : ')
+    if way == '':
+        raise ValueError()
+except ValueError:
+    print('입력하지 않았습니다. 다시 실행해주세요')
+    exit()
 
-match way:
-    case '메일':
-        # processing - send email
-        to_addr = input("이메일 : ")
-        sendEmail(to_addr, book)
-        
-    case '파일':
-        # processing - save file
-        pass
+# save or send
+try :
+    match way:
+        case '메일':
+            # send email
+            to_addr = input("이메일 : ")
+            sendEmail(to_addr, book)
+        case '파일':
+            # save file
+            to_name = input('파일 이름 : ')
+            saveFile(to_name, book)
+except Exception:
+    print("에러가 발생했습니다")
